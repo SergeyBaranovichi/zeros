c177d14a9a5609dbc86fc484b2970aaee217ebf8
SergeyBaranovichi <gsergey1998@gmail.com>
2018-09-28 01:37:14 +0300

    zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+var a=0;
+for (var i=1; i<=number; i++){
+	var b=i;
+	if (b%5==0){
+		
+		while (i>0){
+			if (b%5==0){
+				a++;
+				b=b/5;
+			} else break;
+		}
+		}
+	}
+	return a;
 }
